525bffb566a4e597ffaf6398276328221c8c59ce
Rignak <aureliencolin@hotmail.com>
2016-11-07 21:43:59 +0100

    Add files via upload

--- DanbooruPost.py
+++ DanbooruPost.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Created : 26/08/2016
-Last modified : 09/09/2016
 Python version : 3.5
 @author: Rignak
 """
@@ -12,8 +11,8 @@ from os import remove, rename
 # Declare each files wich will be used
 root = "E:\Telechargements\Anime\post\data"
 error_fname = "error.txt"
-list_T = ["T1.txt", "T2.txt", "T3.txt", "Priority.txt"]
-list_L = ["L1.txt", "L2.txt", "L3.txt", "LPriority.txt"]
+list_T = ["T1.txt", "T2.txt", "T3.txt", "T4.txt", "Priority.txt"]
+list_L = ["L1.txt", "L2.txt", "L3.txt", "L4.txt", "LPriority.txt"]
 list_HTML = []
 for i in range(len(list_T)):
     list_T[i] = join(root, list_T[i])
@@ -26,6 +25,18 @@ error_f = open(join(root, error_fname), "r")
 for line in error_f:
     l = line.split()
     dic_error[l[0]] = l[1]
+
+def CorrectorSample(url_sample):
+    """Return the url of the picture from the sample
+    Input:
+    url_sample - string, the url in the .html or .txt
+
+    Output:
+    url_pict - string, the url of the image"""
+
+    if url_sample.startswith("https://files.yande.re/sample/"):
+        return url_sample.replace('sample', 'image')
+    return url_sample
 
 
 def ReplaceAll(text, dic_error):
@@ -45,6 +56,8 @@ def DelLine(list_n):
     """Delete line in the html file
     Input:
     list_n - A list of str, number of lines to delete in each file"""
+    while len(list_n) < len(list_HTML):
+        list_n.append(0)
     for i in range(len(list_HTML)):
         newHTML = open(list_HTML[i] + '~', 'w')
         oldHTML = open(list_HTML[i], 'r')
@@ -90,7 +103,7 @@ def GenerateTXT():
         for line in file:
             try:
                 lineL = line.split("\"")[-2] + "\n"
-                fileL.write(lineL)
+                fileL.write(CorrectorSample(lineL))
                 lineT = line.split("\"")[-1][2:] + "--\n"
                 fileT.write(ReplaceAll(lineT, dic_error))
             except:
@@ -108,15 +121,27 @@ def GenerateHTML():
             lineT = fileT.readline()
             fileT.readline()
             file.write(
-                '<A HREF="' + lineL[:-1] + '"> ' + ReplaceAll(lineT, dic_error) + '<br/><br/>')
+                '<A HREF="' + CorrectorSample(lineL[:-1]) + '"> ' + ReplaceAll(lineT, dic_error) + '<br/><br/>')
     return
 
+def CountLine():
+    """Count the number of line of each .txt"""
+    i = []
+    for file in list_L:
+        j = 0
+        current = open(file, 'r')
+        for line in current:
+            j += 1
+        print(j)
+        i.append(j)
+    return
 
 if __name__ == '__main__':
     print("Case 1 : Generate HTML")
     print("Case 2 : Generate TXT")
     print("Case 3 : Delete lines from trim.txt")
-    print("Case 4 : Delete n, m, k, l first line")
+    print("Case 4 : Delete n, m, k, l, p first line")
+    print("Case 5 : Count the line in each link file")
     case = int(input('Choisir un mode : '))
     if case == 1:
         GenerateHTML()
@@ -129,3 +154,5 @@ if __name__ == '__main__':
         list_n = input("How many line to delete? ")
         DelLine(list_n.split())
         GenerateTXT()
+    elif case == 5:
+        CountLine()
