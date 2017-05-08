739d4c5bf89b753122cf8ec86722279e3eae40c9
Aurelien COLIN <aurelien.colin@imt-atlantique.net>
2017-05-08 23:36:59 +0200

    Add estimation of end time

--- Danbooru/AddTags/DelFav.py
+++ Danbooru/AddTags/DelFav.py
@@ -8,10 +8,11 @@ It shows the image and allow to decide if it should be removed from fav
 It write the link in a .txt, to manually double-check and unfav
 """
 
-from AddTags import *
+from AddTags import ListImgs, ListUrl, Sample
+from datetime import datetime
 
-limit = 2000
-inf = 1630287
+limit = 1500
+inf = 1969904
 sup = 1000000000
 f = open("../Danbooru_Codes.txt")
 api_key = f.readline().split()[1]
@@ -22,18 +23,21 @@ data = []
 for i in range((limit-1)//100+1):
     print('Searching for picts:', i+1, 'on', limit//100 + 1)
     data += ListUrl(tags+str(inf))
-    inf = data[-1]['id']
+    inf = int(data[-1]['id'])+1
     if inf>sup:
         break
 imgs = ListImgs(data)
 r_file = open('to_del.txt', 'w')
+input('Push enter to begin')
+begin = datetime.now()
 for img in imgs:
     i += 1
-    print(i, '-', img._Id)
+    print(i, '-', img._Id, '|',
+              ((datetime.now()-begin)/(i+1)*(len(imgs)-i) + datetime.now()).strftime('%H:%M'))
     img._add = Sample.InputTags(img)
     if img._add == 'return':  # End of operations
         break
     elif img._add == 'l' :
-        r_file.write(str(img._Id) + "\n")
+        r_file.write('http://danbooru.donmai.us/posts/'+str(img._Id) + "\n")
 r_file.close()
 
