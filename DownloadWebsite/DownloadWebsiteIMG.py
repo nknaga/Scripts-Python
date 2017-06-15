d6f30084e14034fbecdd367ce51d51a7dc18d76d
Aurelien COLIN <aurelien.colin@imt-atlantique.net>
2017-06-15 16:34:46 +0200

    Major modifcations on the stucture. Add threading to raise speed.

--- DownloadWebsite/DownloadWebsiteIMG.py
+++ DownloadWebsite/DownloadWebsiteIMG.py
@@ -1,11 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+    Author: Rignak
+    Python version: 3.5
+"""
 
-
-from os import makedirs, walk
 import urllib
 import bs4 as BeautifulSoup
 from datetime import datetime
-from os.path import join, dirname, realpath
-
+from threading import Thread
+import threading
+import time
+from os import makedirs
 
 def EndTime(begin, i, n):
     mean_time = (datetime.now()-begin)/i
@@ -14,58 +19,73 @@ def EndTime(begin, i, n):
     return end_time.strftime('%H:%M')
 
 def GetDataLink(link):
-    e = 1
-    while e:
+    n = 0
+    while n < 5:
         try:
             page = urllib.request.urlopen(link)
-            e = 0
-        except urllib.error.HTTPError:
-            e = 1
-            continue
-        try:
             bytespage = page.read()
             soup = BeautifulSoup.BeautifulSoup(bytespage, "lxml")
-            e = 0
-        except:
-            e = 1
-    present_links = soup.find_all('a', href=True)
-    return present_links
+            n = 7
+        except Exception as e:
+            n += 1
+    if n == 7:
+        present_links = soup.find_all('a', href=True)
+        return present_links
+    else:
+        return []
 
 
 def GetDataWebsite(site):
-
-    visited_links = []
-    unvisited_links = [site]
+    if site == '':
+        return
+    global visited
+    global unvisited
+    global pictures_id
+    visited = []
+    unvisited = [site]
     pictures_id = {}
+    limit_active = 100+threading.active_count()
+
     begin = datetime.now()
+    last = begin
+    ini_thread = threading.active_count()
+    while unvisited or threading.active_count() != ini_thread:
+        while not unvisited:
+            if threading.active_count() == ini_thread:
+                return
+            elif (datetime.now()-last).total_seconds() > 300:
+                return
+            else:
+                time.sleep(0.5)
+        while threading.active_count() > limit_active:
+            time.sleep(0.5)
+        current_link = unvisited.pop(0)
+        visited.append(current_link)
+        Thread(target=IndividualGetData, args=(current_link, site)).start()
+        if (datetime.now()-last).total_seconds() > 10:
+            l1, l2, l3 = [len(x) for x in [visited, unvisited, pictures_id]]
+            print('V|R[P:', l1,'|',l2,'|', l3,'|', EndTime(begin, l1, l1+l2))
+            last = datetime.now()
 
-    while unvisited_links:
-        current_link = unvisited_links[0]
-        unvisited_links.pop(0)
-        visited_links.append(current_link)
-
-        present_links = GetDataLink(current_link)
-
-        for i, data in enumerate(present_links):
-            href = data['href'].split('?')[0]
-            if href.startswith(site) and href not in visited_links and href not in unvisited_links:
-                unvisited_links.append(href)
-                continue
-            if href.endswith("Render.png"):
-                ID = href.split('/')[-1]
-                if ID not in pictures_id:
-                    pictures_id[ID] = href
-                else:
-                    if href.split('/')[-2][1:] > pictures_id[ID].split('/')[-2][1:]:
-                        pictures_id[ID] = href
-        print('V|R[P|E:', len(visited_links),'|',len(unvisited_links),'|',
-              len(pictures_id),'|', EndTime(begin, len(visited_links),
-                                            len(visited_links)+len(unvisited_links)),
-              '|', current_link)
-
+    time.sleep(120)
     pictures = [pictures_id[ID] for ID in pictures_id]
     print(pictures)
     return pictures
+
+def IndividualGetData(url, site):
+    global visited
+    global unvisited
+    present_links = GetDataLink(url)
+    for i, data in enumerate(present_links):
+        href = data['href'].split('?')[0].split('#')[0]
+        if href.startswith(site) and href not in visited and href not in unvisited:
+            unvisited.append(href)
+        if href.endswith(".png"):
+            ID = href.split('/')[-1]
+            if ID not in pictures_id:
+                Thread(target=IndividualDownload, args=(href,)).start()
+                pictures_id[ID] = href
+
 
 def Download(pictures):
     try:
@@ -73,38 +93,20 @@ def Download(pictures):
     except Exception:
         pass
     begin = datetime.now()
+    limit_active = 100
     for i, url in enumerate(pictures):
-        try:
-            urllib.request.urlretrieve(url, "Images/" + url.split('/')[-1])
-        except:
-              pass
+        while threading.active_count() > limit_active:
+            time.sleep(0.5)
+        Thread(target=IndividualDownload, args=(url,)).start()
         print(i+1, "on", len(pictures), '|',EndTime(begin, i+1, len(pictures)))
 
+def IndividualDownload(url):
+    try:
+        url = url.replace('-1.png', '.png').replace('-2.png', '.png').replace('-3.png', '.png')
+        urllib.request.urlretrieve(url, "Images/" + url.split('/')[-1])
+    except:
+        pass
+
 if __name__ == '__main__':
-    """site = "http://ors-renders.animemeeting.com/"
-    Download(pictures)"""
-
-    standartRoot = "E:\\Telechargements\\Anime\\ors\\ors"
-    s = open(join(dirname(realpath(__file__)), "samples.txt"), 'r')
-    pictures = []
-    full = {}
-    for line in s:
-        pictures.append(line.split('/')[-1])
-        full[pictures[-1]] = line
-
-    f = open(join(dirname(realpath(__file__)), "txt.txt"), 'w')
-    names = []
-    for root, dirs, files in walk(standartRoot):
-        for i, name in enumerate(files):
-            if name.endswith("_noise.png"):
-                name = name.replace("_noise.png", ".png")
-            names.append(name)
-    for name in names:
-        for picture in pictures:
-            if name in picture:
-                #print(picture)
-                l= "<DT><A HREF=\"" + full[picture][:-1] + "\"ADD_DATE=\"1487861288\">image</A>\n"
-                print(l)
-                f.write(l)
-                continue
-    f.close
+    GetDataWebsite(input('Enter a website: '))
+    #Download(GetDataWebsite(input('Enter a website: ')))
