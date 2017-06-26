3f2e8b87e07589f3f1bc07eaed0f0153328deaec
Aurelien COLIN <aurelien.colin@imt-atlantique.net>
2017-06-26 23:50:41 +0200

    Programmes to extract data on anime and make graph

---
+++ Myanimelist/MakeGraph.py
@@ -0,0 +1,139 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 26 11:27:34 2017
+
+@author: Rignak
+"""
+
+from urllib.request import urlopen
+import bs4
+import json
+import matplotlib.pyplot as plt
+
+def UserListCompleted(username):
+    url = 'https://myanimelist.net/animelist/' + username + '?status=2'
+    soup = bs4.BeautifulSoup(urlopen(url).read(), 'lxml')
+    fa = soup.find_all('a')
+    ids = []
+    for a in fa:
+        if 'href' in a.attrs.keys() and a.attrs['href'].startswith('/anime/'):
+            ids.append(int(a.attrs['href'].split('/anime/')[1].split('/')[0]))
+    return ids
+
+def LoadDic():
+    with open('myanimelist.json', 'r') as file:
+        data = json.load(file)
+    return data
+
+def ReduceDic(full_dic, ids):
+    user_dic = {}
+    for key in ids:
+        if str(key) in full_dic:
+            user_dic[str(key)] = full_dic[str(key)]
+    return user_dic
+
+def CheckCondition(entry, c):
+    key, value = c
+    key += ':'
+    if key not in entry:
+        return False
+    if len(entry[key]) == 1:
+        if entry[key] == value:
+            return True
+        else:
+            return False
+    elif value in entry[key]:
+        return True
+    else:
+        return False
+
+def ReduceOnConditions(dic, conditions):
+    res = {}
+    for key, value in dic.items():
+        ok = True
+        for condition in conditions:
+            c = CheckCondition(value, condition)
+            if not c:
+                ok = False
+                break
+        if ok:
+            res[key] = value
+    return res
+
+def CountYear(dic, years):
+    nbs = []
+    for year in years:
+        nb = 0
+        for key, value in dic.items():
+            if not 'Aired:' in value or not value['Aired:']:
+                continue
+            if 'Aired:' in value and value['Aired:'][0].split('/')[-1] == '?':
+                continue
+            try:
+                if 'Aired:' in value and int(value['Aired:'][0].split('/')[-1])==year:
+                    nb+=1
+            except:
+                pass
+                #print('error:', value['Aired:'])
+        nbs.append(nb)
+    return nbs
+
+def GetData():
+    years = range(1970, 2018)
+    data0 = LoadDic()
+    nbs_total = CountYear(data0, years)
+    print('You can choose conditions from :\n', '\n'.join(list(data0['1'].keys())))
+
+    conditions = input("Conditions (split on '&') ? ")
+    if conditions:
+        conditions = [c.split(':') for c in conditions.split('&')]
+        nbs_all = CountYear(ReduceOnConditions(data0, conditions), years)
+
+        mode = input('Result in nb or in % ? ')
+        if mode == '%':
+            ylabel = '%'
+            p_all = GetProportion(nbs_all,nbs_total)
+            data = [p_all]
+        else:
+            ylabel = 'nb'
+            data = [nbs_all]
+        legend = ['all']
+
+        username = input('Username ? ')
+        if username:
+            data_user = ReduceDic(data0, UserListCompleted('Rignak'))
+            nbs_user = CountYear(ReduceOnConditions(data_user, conditions), years)
+
+            if mode == '%':
+                p_user = GetProportion(nbs_user,nbs_total)
+                p_relatif = GetProportion(nbs_user,nbs_all)
+                data += [p_user, p_relatif]
+                legend += ['user', 'relatif']
+            else:
+                data += [nbs_user]
+                legend += ['user']
+    else:
+        ylabel = 'nb'
+        data, legend = [nbs_total], ['all']
+
+    Plt(years, data, legend, ylabel)
+
+def GetProportion(l1, l2):
+    p = []
+    for i in range(len(l2)):
+        if l2[i]:
+            p.append(l1[i]/l2[i]*100)
+        else:
+            p.append(0)
+    return p
+
+def Plt(x, data, legend, ylabel):
+    fig, ax1 = plt.subplots()
+    for y in data:
+        ax1.plot(x, y)
+    ax1.set_xlabel('Years')
+    ax1.set_ylabel(ylabel)
+    plt.legend(legend, loc="best")
+
+if __name__ == '__main__':
+    GetData()
---
+++ Myanimelist/MakeJSON.py
@@ -0,0 +1,170 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 23 13:31:34 2017
+
+@author: Rignak
+"""
+
+import bs4
+import json
+from datetime import datetime
+from threading import Thread
+import requests
+import time
+import threading
+
+base = 'https://'+'/'.join(['myanimelist.net','anime'])
+headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:43.0) Gecko/20100101 Firefox/43.0'}
+
+def GetTuple(soup, key):
+    values = []
+    for div in soup.find_all('div'):
+        span = div.find('span')
+        if span and str(span.string) == key:
+            fa = div.find_all('a')
+            if fa:
+                for a in fa:
+                    values.append(str(a.string))
+            else:
+                s = str(div).split('</span>')[-1].split('</div>')[0].split('\n')[1][2:]
+                values.append(s)
+    if len(values) ==1:
+        return values[0]
+    return values
+
+def Month2Int(string):
+    if string[-1] == ',':
+        string = string[:-1]
+    if string == 'Jan':
+        return '01'
+    if string == 'Feb':
+        return '02'
+    if string == 'Mar':
+        return '03'
+    if string == 'Apr':
+        return '04'
+    if string == 'May':
+        return '05'
+    if string == 'Jun':
+        return '06'
+    if string == 'Jul':
+        return '07'
+    if string == 'Aug':
+        return '08'
+    if string == 'Sep':
+        return '09'
+    if string == 'Oct':
+        return '10'
+    if string == 'Nov':
+        return '11'
+    if string == 'Dec':
+        return '12'
+    return string
+
+def FormatD(string):
+    string = string.replace(',', '')
+    if len(string) == 1:
+        return '0'+string
+    return string
+
+def Date(value):
+    date = None
+    try:
+        date = value.split()
+        d1, m1, y1, d2, m2, y2 = '01', '01', '1000', '01', '01', '1000'
+        if len(date) == 1:
+            y1 = date[0]
+        elif len(date) == 2:
+            if value != 'Not available':
+                m1, y1 = date
+        elif len(date) == 3:
+            if date[-1] != '?':
+                m1, d1, y1 = date
+            else:
+                y1 = date[0]
+        elif len(date) == 4 and date[-2] == 'to':
+            m1, y1 = date[0:2]
+            if date[-1] != '?':
+                y2 = date[-1]
+        elif len(date) == 5:
+            if date[2] == 'to':
+                m1, y1, to, m2, y2 = date
+            else:
+                d1, m1, y1 = date[0:3]
+        elif len(date) == 6:
+            if date[2] == 'to':
+                m1, y1, to, m2, d2, y2 = date
+            elif date[3] == 'to':
+                m1, d1, y1, to, m2, y2 = date
+        elif len(date) == 7:
+            m1, d1, y1, to, m2, d2, y2 = date
+        date = ['/'.join([FormatD(d1), Month2Int(m1), y1])]
+        if len(date) > 3:
+            date += ['/'.join([FormatD(d2), Month2Int(m2), y2])]
+    except Exception as e:
+        print('Error on date:', value, e)
+    return date
+
+def GetTitle(soup):
+    return str(soup.find('title').string).split(' - MyAnimeList.net')[0].split('\n')[1]
+
+def IndividualNb(nb, keys):
+    global anime
+    global find
+    anime[nb] = {}
+    try:
+        url = '/'.join([base, str(nb)])
+        req = requests.get(url, headers=headers, stream=True)
+        while req.status_code == 429:
+            time.sleep(20)
+            req = requests.get(url, headers=headers, stream=True)
+        if req.status_code == 404:
+            anime.pop(nb)
+            return
+        soup = bs4.BeautifulSoup(req.content, 'lxml')
+    except:
+        print(Exception)
+        return
+    for key in keys:
+        anime[nb][key] = GetTuple(soup, key)
+    anime[nb]['Aired:'] = Date(anime[nb]['Aired:'])
+    anime[nb]['name'] = GetTitle(soup)
+    find += 1
+
+def CreateDic():
+    global anime
+    global find
+    anime, find = {}, 0
+    l = list(range(1,36000))
+    limit = len(l)
+    keys = ['Episodes:', 'Type:', 'Premiered:', 'Genres:',
+            'Aired:', 'Broadcast:', 'Producers:', 'Licensors:', 'Source:',
+            'Studios:', 'Duration:']
+    begin = datetime.now()
+    last, now = begin, begin
+    limit_active = 20 + threading.active_count()
+    try:
+        p = None
+        for nb in l:
+            while threading.active_count() > limit_active:
+                time.sleep(0.5)
+            Thread(target=IndividualNb, args=(nb, keys)).start()
+            now = datetime.now()
+            if str(int(nb/limit*1000)/10) != p:
+                k = nb+1
+                p = str(int(nb/limit*1000)/10)
+                ending = ((now - begin)/k*limit + begin).strftime('%D-%H:%M')
+                print(p+'%', '|', ending, '|', find)
+
+            if (now-last).total_seconds()>10:
+                last = now
+    except:
+        pass
+    finally:
+        time.sleep(10)
+        print('MEAN TIME:', (now-begin)/limit)
+        with open('myanimelist.json', 'w') as file:
+            json.dump(anime, file, sort_keys=True, indent=4)
+
+if __name__ == '__main__':
+    CreateDic()
