6c551578e1f53ce07ca9d764dca5c3566463401d
Aurelien COLIN <aurelien.colin@imt-atlantique.net>
2017-06-29 18:41:45 +0200

    Month2Int : simplicate the function

--- Myanimelist/MakeJSON.py
+++ Myanimelist/MakeJSON.py
@@ -35,30 +35,10 @@ def GetTuple(soup, key):
 def Month2Int(string):
     if string[-1] == ',':
         string = string[:-1]
-    if string == 'Jan':
-        return '01'
-    if string == 'Feb':
-        return '02'
-    if string == 'Mar':
-        return '03'
-    if string == 'Apr':
-        return '04'
-    if string == 'May':
-        return '05'
-    if string == 'Jun':
-        return '06'
-    if string == 'Jul':
-        return '07'
-    if string == 'Aug':
-        return '08'
-    if string == 'Sep':
-        return '09'
-    if string == 'Oct':
-        return '10'
-    if string == 'Nov':
-        return '11'
-    if string == 'Dec':
-        return '12'
+    corr = {'Jan':'01','Feb':'02','Mar':'03','Apr':'04','May':'05','Jun':'06',
+            'Jul':'07','Aug':'08','Sep':'09','Oct':'10','Nov':'11','Dec':'12'}
+    if string in corr:
+        return corr[string]
     return string
 
 def FormatD(string):
