a1d25fdccbbcf282ddda7a239601cfa91bfae98a
Aurelien COLIN <aureliencolin@hotmail.com>
2017-08-05 23:08:28 +0200

    Record video in x265

--- ToDoManager/ToDoManager.py
+++ ToDoManager/ToDoManager.py
@@ -17,28 +17,29 @@ def FromYoutube(line):
         name, link, ext, begin, end = line
     yt = pytube.YouTube(link)
     yt.set_filename(name)
-    yt.filter('mp4')[-1].download(join(local, 'mp3'))
+    yt.filter('mp4')[-1].download(join(local, ext))
     if ext == 'mp3':
         line = name + '.mp4\t' + begin + '\t' + end
         ToMP3(name + '.mp4\t' + begin + '\t' + end)
 
-def CutMP4(line):
+def CutVideo(line):
     l1 = line.split('\t')
     l2 = []
     for x in l1:
         if x:
             l2.append(x)
     res_name, begin, end, epi, path = l2
-    res_name = join(local, 'ok', res_name +'.mp4')
-    path = join(local, 'mp4', path)
+    res_name = join(local, 'ok', res_name +'.mkv')
+    path = join(local, 'mkv', path)
     begin, end = [ConvertTime(d) for d in [begin, end]]
     if end != '00:00:00':
         end =  " -to " + end
     else:
         end = ""
-    opt = " -c copy -c:s mov_text "
-    os.system('ffmpeg.exe -i "' + path + '" -ss ' + begin + end \
-                + opt +'"' + res_name + '"')
+    opt = " -c:v libx265 -map 0 -force_key_frames 0  -crf 23 "
+    line = 'ffmpeg.exe -i "' + path + '" -ss ' + begin + end \
+                + opt +'"' + res_name + '"'
+    os.system(line)
 
 def ToMP3(line):
     name, begin, end = line.split('\t')
@@ -50,6 +51,8 @@ def ToMP3(line):
         end =  " -to " + end
     else:
         end = ""
+    while os.path.exists(res):
+        res+='1'
     os.system('ffmpeg.exe -i "' + name + '" -ss ' + begin + end + ' "' + res + '"')
 
 def ConvertTime(d):
@@ -64,25 +67,25 @@ if __name__ == '__main__':
     local = "E:\Telechargements\Anime\\to do"
     with open(join(local, 'To Do.txt'), 'r') as file:
         lines = [line.replace('\n', '') for line in file.readlines()]
-    youtube, mp4, mp3 = False, False, False
+    youtube, video, mp3 = False, False, False
     for line in lines:
         if line.startswith('youtube'):
             youtube = True
             continue
         elif line.startswith('video'):
             youtube = False
-            mp4 = True
+            video = True
             continue
         elif line.startswith('audio'):
-            mp4 = False
+            video = False
             mp3 = True
             continue
-        elif line.startswith('--') or not (mp4 or mp3 or youtube):
+        elif line.startswith('--') or not (video or mp3 or youtube):
             continue
         if youtube:
             FromYoutube(line)
-        elif mp4:
-            CutMP4(line)
+        elif video:
+            CutVideo(line)
         elif mp3:
             ToMP3(line)
         print(line.split('\t')[0])
