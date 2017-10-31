1519737f517a50aac3260372965b6faca5e0e6c4
Aurelien COLIN <aureliencolin@hotmail.com>
2017-10-31 13:04:11 +0100

    Externalyse getting danbooru codes and numer of images.

--- Danbooru/StatsDan/stats_dan.py
+++ Danbooru/StatsDan/stats_dan.py
@@ -5,67 +5,36 @@ Created on Tue Jul 19 12:13:11 2016
 @author: Rignak
 """
 
-import urllib
-from datetime import timedelta, date, datetime
-from time import sleep
 
-str1 = "http://sonohara.donmai.us/counts/posts.json?tags="
-f = open("../Danbooru_Codes.txt")
-api_key = f.readline().split()[1]
-username = f.readline().split()[1]
-f.close()
-
-def NbTags(tags):
-    request = str1 + tags + '&login=' + username + '&api_key=' + api_key
-    e = True
-    while e:
-        try:
-            e = False
-            page = urllib.request.urlopen(request)
-        except urllib.error.HTTPError:
-            print('Spam?')
-            sleep(300)
-            e = True
-
-    bytespage = page.read()
-    deleted_number = int(bytespage.decode('utf-8')[21:-2])
-    return deleted_number
-
-
+from datetime import timedelta, date
+from lib import Lib
 
 if __name__ == '__main__':
-    f = open("../Danbooru_Codes.txt")
-    api_key = f.readline().split()[1]
-    username = f.readline().split()[1]
-    f.close()
-
-    begin = datetime.now()
+    api_key, username = Lib.DanbooruCodes()
     mini = 0
     maxi = 50
     file = open('result.txt', 'w')
     for i in range(mini, maxi):
         res = str(date.today() - timedelta(days=i)) + " "
         tag_date = "%20age:" + str(i) + "d"
-        upload_number = NbTags("approver:any" + tag_date)
+        upload_number = Lib.NbTags("approver:any" + tag_date, username, api_key)
         if i < 4:
-            upload_number += NbTags("status:pending"+tag_date)
+            upload_number += Lib.NbTags("status:pending"+tag_date, username, api_key)
         if upload_number == 0:
             deleted_number = 0
             ratio = 1
         else:
-            deleted_number = NbTags("status:deleted%20approver:none" + tag_date)
+            deleted_number = Lib.NbTags("status:deleted%20approver:none" + tag_date, username, api_key)
             upload_number += deleted_number
             ratio = deleted_number/upload_number
         res += str(ratio).replace('.', ',') + " "
         res += str(upload_number) + " "
         if i<620:
-            user_number = NbTags("user:" + username + "%20status:all" + tag_date)
+            user_number = Lib.NbTags("user:" + username + "%20status:all" + tag_date, username, api_key)
             if user_number == 0:
                 user_del = 0
             else:
-                user_del = NbTags("user:" + username + "%20status:deleted" + tag_date)
+                user_del = Lib.NbTags("user:" + username + "%20status:deleted" + tag_date, username, api_key)
             res += str(user_number) + " "
             res += str(user_del)
-
-        t_mean = (datetime.now()-begin)/(i+1)
         print(res)
