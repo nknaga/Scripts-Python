b66de6e667f564365c659fd03d6a1b02ece8395d
Aurelien COLIN <aureliencolin@hotmail.com>
2017-10-31 13:03:46 +0100

    Speed up by using thread.

--- Danbooru/PicturesUtil/DownloadDanbooru.py
+++ Danbooru/PicturesUtil/DownloadDanbooru.py
@@ -10,11 +10,20 @@ import requests
 import shutil
 from os.path import join
 import time
-f = open("../Danbooru_Codes.txt")
-api_key = f.readline().split()[1]
-username = f.readline().split()[1]
-f.close()
-    
+import os
+from datetime import datetime
+from sys import stdout
+from threading import Thread
+from PIL import Image
+import threading
+
+def Progress(s):
+    stdout.write('\r')
+    stdout.write(s+'           ')
+    stdout.flush()
+
+
+
 def ListPicturesWithTag(tags, limit):
     """ Return a list of 'limit1' url, with the tags 'tags'
 
@@ -43,23 +52,43 @@ def ListPicturesWithTag(tags, limit):
 
 
 def Launch():
-    tags = input("Write some tags: ")
-    limit = int(input("Number of pictures with the tags: "))
-    urls = ListPicturesWithTag(tags, limit)
-    for i, url in enumerate(urls):
-        if i > limit:
-            break
-        print(i, '|', len(urls), '|', url)
-        ok = False
-        while not ok:
-            try:
-                r = requests.get(url, stream=True)
-                ok = True
-            except Exception as e:
-                print(e)
-                time.sleep(10)
-        if r.status_code == 200:
-            with open(join('result', str(i) + '.' + url.split('.')[-1]), 'wb') as f:
-                r.raw.decode_content = True
-                shutil.copyfileobj(r.raw, f)
-Launch()
+    limit_active = int(input("Thread number : "))
+    tags = input("Write some tags (split search with |): ").split('|')
+    for tag in tags:
+        try:
+            os.makedirs(join('result', tag.split()[-1]))
+        except Exception as e:
+            print(e)
+    limits = [int(i) for i in input("Number of pictures with the tags (split with blank): ").split()]
+    if len(tags)!=len(limits):
+        print('Error : not same length')
+        return
+    total = sum(limits)
+    begin = datetime.now()
+    k = 0
+    for j in range(len(tags)):
+        urls = ListPicturesWithTag(tags[j], limits[j])
+        folder = tags[j].split()[-1]
+        for i, url in enumerate(urls):
+            k +=1
+            if i > limits[j]:
+                break
+            while threading.active_count() > limit_active:
+                time.sleep(0.1)
+            Thread(target=Download, args=(i, folder, url)).start()
+            ending = (datetime.now() - begin) / k  * total + begin
+            Progress(str(k) + ' on ' + str(total) + ' | ' + ending.strftime('%H:%M'))
+
+
+def Download(i, folder, url):
+    r = requests.get(url, stream=True)
+    if r.status_code == 200:
+        with open(join('result', folder, str(i) + '.' + url.split('.')[-1]), 'wb') as f:
+            r.raw.decode_content = True
+            shutil.copyfileobj(r.raw, f)
+
+if __name__ == '__main__':
+    with open("../Danbooru_Codes.txt") as f:
+        api_key = f.readline().split()[1]
+        username = f.readline().split()[1]
+    Launch()
