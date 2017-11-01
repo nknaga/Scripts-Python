e73404f44e320fe8f50f2025de9d4b8e27885ac6
Aurelien COLIN <aureliencolin@hotmail.com>
2017-11-01 15:59:35 +0100

    Speed up with thread. Nb of tag per page 1000 (up from 20). Add aliases.

--- Danbooru/AddTags/tags.py
+++ Danbooru/AddTags/tags.py
@@ -6,24 +6,69 @@ Created on Wed Nov  2 21:50:59 2016
 """
 import urllib
 import bs4 as BeautifulSoup
+from sys import stdout
+from datetime import datetime
+from time import sleep
+from threading import Thread
+import threading
 
-url_beg = "http://sonohara.donmai.us/tags?commit=Search&page="
-url_end = "&search%5Bcategory%5D=0&search%5Bhide_empty%5D=yes&search%5Border%5D=count&utf8=%E2%9C%93"
+def Progress(s):
+    stdout.write('\r')
+    stdout.write(s+'            ')
+    stdout.flush()
 
-tags = []
-for i in range(500):
-    print(i)
-    url = url_beg + str(i+1) + url_end
-    page = urllib.request.urlopen(url)
-    soup = BeautifulSoup.BeautifulSoup(page, "lxml")
+def DanbooruCodes():
+    with open("../Danbooru_Codes.txt", 'r') as f:
+        api_key = f.readline().split()[1]
+        dan_username = f.readline().split()[1]
+    return api_key, dan_username
+
+def IndividualRequest(url,i):
+    global tags
+    ok = False
+    while not ok:
+        try:
+            page = urllib.request.urlopen(url)
+            soup = BeautifulSoup.BeautifulSoup(page, "lxml")
+            ok = True
+        except:
+            pass
     td = soup.find_all('td')
+    if i == 0:
+        j = -3
+    elif i == 1:
+        j = 2
     for ele in td:
-        tag = str(ele).split('>')[-3][:-3]
-        if tag.startswith('<'):
-            continue
-        if len(tag)<4:
-            continue
-        tags.append(tag)
+        tag = str(ele).split('>')[j][:-3]
+        if tag and not tag.startswith('<') and tag!='fix' and tag != 'show':
+            tags.append(tag)
 
-f = open('tags.txt', 'w')
-f.write("\n".join(tags))
+    
+if __name__ == '__main__':
+    urls_beg = ["http://sonohara.donmai.us/tags?commit=Search&page=",
+               "http://sonohara.donmai.us/tag_aliases?commit=Search&limit=1000&page="]
+    urls_end = ["&search%5Bhide_empty%5D=yes&search%5Border%5D=count&limit=1000",
+               "&search%5Border%5D=tag_count&search%5Bstatus%5D=Approved"]
+    ls = [100, 12]
+    api_key, username = DanbooruCodes()
+    global tags
+    tags = []
+    begin = datetime.now()
+    limit_active = 20
+    for i in range(len(ls)):
+        urls_end[i] += '&login=' + username + '&api_key=' + api_key
+        for j in range(1,ls[i]+1):
+            while threading.active_count() > limit_active:
+                sleep(0.1)
+            url = urls_beg[i] + str(j) + urls_end[i]
+            Thread(target=IndividualRequest, args=(url,i)).start()
+            ending = (datetime.now() - begin) / j * sum(ls) + begin
+            Progress(str(j+ls[0]*i) + ' on ' + str(sum(ls)) + ' | ' + ending.strftime('%H:%M') + ' | ' + str(len(tags)))
+    sleep(20)
+    tags = set(tags)
+    with open('tags.txt', 'w') as f:
+        for tag in tags:
+            try:
+                f.write(tag+'\n')
+            except:
+                print('error:', tag)
