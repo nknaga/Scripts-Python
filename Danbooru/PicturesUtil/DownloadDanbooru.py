ca37aa93b97e261f3a513352bb1c0df711203d8c
Rignak <aureliencolin@hotmail.com>
2018-01-30 15:57:59 +0100

    Fix bugs

--- Danbooru/PicturesUtil/DownloadDanbooru.py
+++ Danbooru/PicturesUtil/DownloadDanbooru.py
@@ -14,15 +14,12 @@ import os
 from datetime import datetime
 from sys import stdout
 from threading import Thread
-from PIL import Image
 import threading
 
 def Progress(s):
     stdout.write('\r')
     stdout.write(s+'           ')
     stdout.flush()
-
-
 
 def ListPicturesWithTag(tags, limit):
     """ Return a list of 'limit1' url, with the tags 'tags'
@@ -35,9 +32,12 @@ def ListPicturesWithTag(tags, limit):
     list_pictures_with -- A list"""
     list_pictures_with = []
     hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11'}
-
-    for i in range(int(limit/200)+1):
-        url = "https://danbooru.donmai.us/posts?page=" + str(i + 1)
+    n = 200
+    begin = datetime.now()
+    for i in range(int(limit/n)+1):
+        eta = ((datetime.now()-begin)/(i+1)*int(limit/n)+begin).strftime('%H:%M')
+        Progress(str(i)+'/'+str(int(limit/n))+' | '+tags+' | '+eta)
+        url = "http://hijiribe.donmai.us/posts?page=" + str(i + 1)
         url = url + "&tags=limit%3A200+" + tags.replace(' ','+')
         url += "&login="+username+"&api_key="+api_key
         req = urllib.request.Request(url, headers=hdr)
@@ -47,16 +47,21 @@ def ListPicturesWithTag(tags, limit):
         for sample in soup.find_all('article'):
             entry = "https://danbooru.donmai.us/"
             entry = entry + sample.get("data-file-url")
+            #entry = entry + sample.get("data-large-file-url")
             list_pictures_with.append(entry)
     return list_pictures_with
 
 
 def Launch():
-    limit_active = int(input("Thread number : "))
+    limit_active = int(input("Thread number : ")) + threading.active_count()
     tags = input("Write some tags (split search with |): ").split('|')
     for tag in tags:
         try:
-            os.makedirs(join('result', tag.split()[-1]))
+            replace = ('>', '<', '?', ':')
+            name = tag.split()[-1]
+            for key in replace:
+                name = name.replace(key, '-')
+            os.makedirs(join('result', name))
         except Exception as e:
             print(e)
     limits = [int(i) for i in input("Number of pictures with the tags (split with blank): ").split()]
@@ -69,6 +74,8 @@ def Launch():
     for j in range(len(tags)):
         urls = ListPicturesWithTag(tags[j], limits[j])
         folder = tags[j].split()[-1]
+        for key in replace:
+            folder = folder.replace(key, '-')
         for i, url in enumerate(urls):
             k +=1
             if i > limits[j]:
@@ -81,11 +88,26 @@ def Launch():
 
 
 def Download(i, folder, url):
-    r = requests.get(url, stream=True)
-    if r.status_code == 200:
-        with open(join('result', folder, str(i) + '.' + url.split('.')[-1]), 'wb') as f:
-            r.raw.decode_content = True
-            shutil.copyfileobj(r.raw, f)
+    if url.endswith('png') or url.endswith('jpg'):
+        url += "?login="+username+"&api_key="+api_key
+        name = str(i)
+        while len(name) < 6:
+            name = '0'+name
+        while folder.endswith('.'):
+            folder = folder[:-1]
+        fullname = join('result', folder, name + '.' + url.split('.')[-1][:3])
+        k = 0
+        while k != 5:
+            r = requests.get(url, stream=True)
+            sc = r.status_code
+            if sc == 200:
+                with open(fullname, 'wb') as f:
+                    r.raw.decode_content = True
+                    shutil.copyfileobj(r.raw, f)
+                break
+            k += 1
+        if not os.path.exists(fullname):
+            print('not created:', url)
 
 if __name__ == '__main__':
     with open("../Danbooru_Codes.txt") as f:
--- Danbooru/PicturesUtil/ShowAndDel.py
+++ Danbooru/PicturesUtil/ShowAndDel.py
@@ -8,20 +8,19 @@ Created on Mon Sep 11 21:33:32 2017
 import os
 from PIL import Image
 
-for file in os.listdir("result"):
-    old_name = os.path.join("result", str(file))
+path = "result\\ok"
+k = 1
+for file in os.listdir(path):
+    old_name = os.path.join(path, str(file))
     img = Image.open(old_name)
     img.show()
     c = input(file[:-4] + ': ' )
     os.system("taskkill /f /im dllhost.exe")
-    if c == 'y':
-        new_name = old_name[:-4] + '_ok' + old_name[-4:]
+    if c == 'm':
+        old_path = old_name.split('\\')[:-1]+['ok']
+        new_name = '\\'.join(old_path) + '\\' + str(k) + old_name[-4:]
         os.rename(old_name, new_name)
+        k+=1
     elif c == 'exit':
         break
-    else:
-        new_name = old_name[:-4] + '_no' + old_name[-4:]
-        os.rename(old_name, new_name)
-        
 
-        
