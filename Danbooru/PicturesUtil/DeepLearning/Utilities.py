c306cc1c9896f87b93fdda3f7a1e66a3810a32e6
Rignak <aureliencolin@hotmail.com>
2018-04-19 15:26:47 +0200

    Add checking name unicity of labels (and inclusion of name: see saber and saber_alter)

--- Danbooru/PicturesUtil/DeepLearning/Utilities.py
+++ Danbooru/PicturesUtil/DeepLearning/Utilities.py
@@ -9,23 +9,50 @@ from  glob import glob
 from PIL import Image
 from os.path import join
 from os import walk
+import sys
 
+root = 'illustrations123'
+    
+def Progress(s):
+    """Print a carriage return then a string
+    
+    Input:
+    s -- a string"""
+    sys.stdout.write('\r')
+    sys.stdout.write(s+'           ')
+    sys.stdout.flush()
+    
 def Checker():
     """Check if all picture are correctly downloaded (no truncated, no 0 size)"""
-    files = glob.glob(join('imgs', 'illus2', '**', '*.jpg'), recursive=True)
+    files = glob(join('imgs', root, '**', '*.jpg'), recursive=True)
     for i, file in enumerate(files):
         try:
             img = Image.open(file)
             img.load()
+            if not i%100:
+                Progress(str(i))
         except Exception as e:
             print('\n-------\n',e, file,'\n-------\n')
             
 def CountFiles():
-    folder = 'illustrations'
     
-    for root, directories, filenames in walk(join('imgs', folder)):
+    for ini, directories, filenames in walk(join('imgs', root)):
         for directory in directories:
-            print(directory, len(glob(join('imgs', folder, '**', directory, '**', '*.jpg'), recursive = True)))
+            print(directory, len(glob(join('imgs', root, '**', directory, '**', '*.jpg'), recursive = True)))
             
+def CheckNameUnicity():
+    allLabels = []
+    for ini, folders, files in walk(join('imgs', root)):
+        for folder in folders:
+            for label in allLabels:
+                if label == folder:
+                    print(label, 'already exist')
+                elif label in folder:
+                    print(label, 'in', folder)
+                elif folder in label:
+                    print(folder, 'in', label)
+            allLabels.append(folder)
+    
 if __name__ == '__main__':
-    CountFiles()
+    CheckNameUnicity()
+    Checker()
