c41309e4de4257ecc317e0c8026bd1e930e8ac4c
Rignak <aureliencolin@hotmail.com>
2018-09-16 11:59:22 +0200

    .

--- DeepLearning/AutoEncoder.py
+++ DeepLearning/AutoEncoder.py
@@ -56,7 +56,7 @@ class PlotLearning(Callback):
         self.losses.append(logs.get('loss'))
         self.val_losses.append(logs.get('val_loss'))
         self.i += 1
-        
+
         plt.figure()
         plt.yscale('log')
         plt.plot(self.x, self.losses, label="Training")
@@ -64,11 +64,11 @@ class PlotLearning(Callback):
         plt.xlabel('Epoch')
         plt.ylabel('cross entropy')
         plt.legend()
-    
+
 
         plt.tight_layout()
         plt.savefig('plot.png')
-        
+
 def ImportModel(weight):
     convBloc = [(64,1), (128,1), (256,1), (512,1)]
     convSize = (5, 5)
@@ -81,7 +81,7 @@ def ImportModel(weight):
     reg = regularizers.l2(weightDecay)
 
     model = Sequential()
-    
+
     # Down convolutions
     for i, (layer, jmax) in enumerate(convBloc):
         for j in range(jmax):
@@ -98,13 +98,13 @@ def ImportModel(weight):
         if j != len(denseLayers)-1:
             model.add(Dropout(0.5))
     model.add(Reshape(reshapeSize))
- 
-    # Up convolutions  
+
+    # Up convolutions
     for i, (layer, jmax) in enumerate(convBloc[::-1]):
         for j in range(jmax):
             model.add(Conv2D(layer, convSize, activation=a, padding=p))
         model.add(UpSampling2D(pooling))
-    
+
     model.add(Conv2D(3, convSize, activation='sigmoid', padding='same'))
 
     sgd = optimizers.SGD(lr=learningRate,momentum=momentum,decay=lrDecay,
@@ -113,28 +113,28 @@ def ImportModel(weight):
     if weight:
         print('load weights')
         model.load_weights(weightPath)
-            
+
     with open('currentModel.txt', 'w') as file:
         old = sys.stdout
         sys.stdout = file
         model.summary()
         sys.stdout = old
-        
+
     return model
-    
-    
+
+
 def Progress(s):
     sys.stdout.write('\r')
     sys.stdout.write(s+'           ')
     sys.stdout.flush()
-    
+
 def PrepareImage(file):
     img = image_utils.load_img(file, target_size=picSize[:2], grayscale=False,
                                interpolation='bicubic')
     x = image_utils.img_to_array(img)
     x = np.expand_dims(x, axis=0)/255
     return x
-        
+
 def PrepareImages(files):
     inputs = []
     begin = datetime.now()
@@ -148,7 +148,7 @@ def PrepareImages(files):
     print()
     inputs = np.array(inputs)
     return inputs
-    
+
 def ListFilesRec(folder):
     resFiles = []
     for root, dirs, files in walk(join('.', 'imgs', folder)):
@@ -157,7 +157,7 @@ def ListFilesRec(folder):
                 resFiles.append(join(root, file))
     resFiles.sort(key =lambda z:int(z.split('(')[1].split(')')[0]))
     return resFiles
-    
+
 
 def Train(inputs):
     model = ImportModel(False)
@@ -166,9 +166,9 @@ def Train(inputs):
              PlotLearning()]
 
     model.fit(epochs=epochs, verbose=1, validation_split = 0.1,
-              x=inputs, y=inputs, batch_size = batchSize, 
+              x=inputs, y=inputs, batch_size = batchSize,
               callbacks=calls)
-              
+
 def Apply(files):
     model = ImportModel(True)
     begin = datetime.now()
@@ -179,24 +179,23 @@ def Apply(files):
 
         im = image_utils.array_to_img(pred[0])
         im.save(join('results', split(file)[-1]))
-        
+
         im = image_utils.array_to_img(x[0])
         im.save(join('results', split(file)[-1][:-4]+'_source'+'.jpg'))
-        
+
         eta = ((datetime.now()-begin)/(i+1)*l+begin).strftime('%H:%M')
         Progress(str(i)+'/'+str(l)+' - '+eta)
     print()
-        
+
 if __name__ == '__main__':
     if len(sys.argv) == 2:
         mode = sys.argv[1]
     else:
         mode = '1'
-    
+
     files = ListFilesRec('fav-rignak')[:]
     if mode == '1':
         Apply(files[:20])
     elif mode == '2':
         inputs = PrepareImages(files)
         Train(inputs)
-    
